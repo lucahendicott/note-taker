d1d64d87cba6c4bae7154854a5bf8d97c0e60349
Lucah Endicott <lucahdavid88@gmail.com>
2020-12-15 16:49:10 -0800

    add post and delete methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { nanoid } = require("nanoid")
 const fs = require('fs')
 const path = require("path");
 const db = path.join(__dirname, "../db/db.json");
@@ -10,8 +11,33 @@ router.get("/notes", (req, res) => {
     });
 });
 
+router.delete("/notes/:id", (req, res) => {
+    fs.readFile(db, "utf8", (err, data) => {
+        if (err) throw err
+        const note = req.params.id;
+        const deleteNote = JSON.parse(data);
+        const newVar = deleteNote.filter((element) => note !== element.id);
+        fs.writeFile(db, JSON.stringify(newVar), (err) => {
+            if (err) return res.JSON({ err: "problem adding" });
+            res.json({ msg: "successfully added" });
+        });
+    });
+});
+
 router.post("/notes", (req, res) => {
-console.log(req.body)
-})
+        function appendNote(req) {
+        fs.readFile(db, "utf8", (err, data) => {
+            if (err) throw err
+            let note = JSON.parse(data)
+            req.id = nanoid()
+            note.push(req)
+            fs.writeFile(db, JSON.stringify(note), (err) => {
+            if (err) return res.JSON({ err: "problem adding" });
+            res.json({ msg: "successfully added" });
+            });
+        })
+    }
+    appendNote(req.body)
+})   
 
 module.exports = router
